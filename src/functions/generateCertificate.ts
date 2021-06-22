bb9207c3b9391fb34e76889253c3ad108f145986
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-22 10:45:45 -0300

    copilar template com handlebars

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,34 @@
-import {document} from "../utils/dynamodbClient"
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import fs from "fs";
+import handlebars from "handlebars";
+import dayjs from "dayjs";
+import { document } from "../utils/dynamodbClient";
 
-interface ICreateCertificate{
-  id: string; 
-  name: string; 
+interface ICreateCertificate {
+  id: string;
+  name: string;
   grade: string;
 }
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compile = async function (data: ITemplate) {
+  const filePath = path.join(process.cwd(), "src", "template", "certificate.hbs");
+
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data);
+}
+
+
+
 export const handle = async (event) => {
 
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -17,7 +41,31 @@ export const handle = async (event) => {
       grade,
     }
   })
-  .promise();
+    .promise();
+
+  const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+
+  const medal = fs.readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    date: dayjs().format("DD/MM/YYY"),
+    grade,
+    name,
+    id,
+    medal,
+  }
+
+  //gerar certificate
+
+  //compilar usando handlebars
+  const content = await compile(data);
+
+  //transformar em PDF
+
+  
+
+  // salvar no s3
+
 
   return {
     statusCode: 201,
